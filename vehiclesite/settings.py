9a351ee4f6ae7ca0ce942a2f35032f265a3655cb
Srikanth <katasrikanth2001@gmail.com>
2021-11-03 19:56:42 +0530

    master

--- vehiclesite/settings.py
+++ vehiclesite/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
